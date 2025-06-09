468bc8edb10e49c627511242de287f4e9ab9847f
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-06-09 00:26:41 -0500

    ahora si

---
+++ src/main/java/co/unicauca/edu/co/compositor/fachadaServicios/IPazYSalvoService.java
@@ -0,0 +1,7 @@
+package co.unicauca.edu.co.compositor.fachadaServicios;
+
+import co.unicauca.edu.co.compositor.fachadaServicios.dtos.StudentStatusDTO;
+
+public interface IPazYSalvoService {
+    StudentStatusDTO verificarEstado(String studentCode);
+}
---
+++ src/test/java/co/unicauca/edu/co/compositor/CompositorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.edu.co.compositor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
